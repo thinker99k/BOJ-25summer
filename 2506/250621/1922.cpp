564cb0d20179a87fecde171f8cc25dbcb259712e
kh <kitsune03k@outlook.com>
2025-06-21 20:55:32 +0900

    dd

---
+++ 2506/250621/1922.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+
+struct edge {
+    ll weight;
+    ll src;
+    ll dst;
+    
+    bool operator<(const edge &other) const {
+        return weight < other.weight;
+    }
+};
+
+vector<edge> edges;
+
+vector<ll> parent(10001, 0);
+
+ll Find(ll v) {
+    if (parent[v] == v) {
+        return v;
+    }
+    else {
+        parent[v] = Find(parent[v]);
+        return parent[v];
+    }
+}
+
+bool Union(ll u, ll v) {
+    ll rootu = Find(u);
+    ll rootv = Find(v);
+    
+    if (rootu != rootv) {
+        if (rootu < rootv) {
+            parent[rootv] = rootu;
+        }
+        else {
+            parent[rootu] = rootv;
+        }
+        // 루트가 같지 않으면 -> union 성공 -> true 반환
+        return true;
+    }
+    // 루트가 같으면 -> union 실패 -> false 반환
+    return false;
+}
+
+ll kruskal() {
+    sort(edges.begin(), edges.end());
+    
+    ll ret = 0;
+    
+    for (auto it: edges) {
+        if (Union(it.src, it.dst)) {
+            ret += it.weight;
+        }
+    }
+    
+    return ret;
+}
+
+int main() {
+    // union-find 준비
+    for (int i = 0; i <= 10000; i++) {
+        parent[i] = i;
+    }
+    
+    ll numv, nume;
+    cin >> numv;
+    cin >> nume;
+    
+    ll weight, src, dst;
+    for (int i = 0; i < nume; i++) {
+        cin >> src >> dst >> weight;
+        edges.push_back({weight, src, dst});
+    }
+    
+    cout << kruskal();
+}
